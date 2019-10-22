76df523e5a4525b3baac0975746c0d6d15d76d08
Varun Date <date.varun123@gmail.com>
2019-10-22 17:43:44 +0530

    base: Lockscreen Date Fonts [1/2]
    SDB: Call SliceView(DateView) Fonts with separate method. It looks neat and clean
    
    Good Job @eldainosor. I was confused because of sliceview shit.
    Bootleggers (@eldainosor) edit: Update the code to use the mKeyguardSlice instead of mDateView (deprecated due to google switching all the ls date section to slices) and make a function to apply the same typeface to every view on such SliceView

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -33,6 +33,7 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.text.LineBreaker;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Trace;
 import android.provider.Settings;
@@ -319,6 +320,16 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         updateTextColors();
     }
 
+    public void setViewsTypeface(Typeface tf) {
+        int childCount = mRow.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View v = mRow.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTypeface(tf);
+            }
+        }
+    }
+
     private void updateTextColors() {
         final int blendedColor = getTextColor();
         mTitle.setTextColor(blendedColor);
@@ -567,7 +578,6 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
 
         @Override
         public void onOverlayChanged() {
-            setTextAppearance(sStyleId);
         }
 
         @Override
